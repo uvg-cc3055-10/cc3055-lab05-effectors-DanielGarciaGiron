16d063abf21635bf959d5646c8af270e3add6231
DanielGarciaGiron <daniel.garcia.giron@gmail.com>
2018-05-02 08:58:37 -0600

    Lab 05

---
+++ Assets/Scripts/AreaEffector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEffector : MonoBehaviour {
+    public GameObject areaEffector1;
+    public GameObject areaEffector2;
+    private float time = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //sumo el tiempo a la variable time.
+        time = time + Time.deltaTime;
+        //enciendo los efectores por 5 segundos.
+        if (time < 5)
+        {
+            areaEffector1.SetActive(true);
+            areaEffector2.SetActive(true);
+        }
+        //los apago por 5 segundos.
+        if(time >= 5 && time < 10)
+        {
+            areaEffector1.SetActive(false);
+            areaEffector2.SetActive(false);
+        }
+        //reseteo el timer.
+        if(time >= 10)
+        {
+            time = 0.0f;
+        }
+	}
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour {
+    public string levelToLoad;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Cuando entra en el colisionador se carga el siguiente nivel. 
+    //El nombre del nivel se guarda en la variable levelToLoad.
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(levelToLoad, LoadSceneMode.Single);
+    }
+}
